5109dfb301547c3a4f170e60fcc1614058b15bc9
ViniciusSouza21 <79486692+ViniciusSouza21@users.noreply.github.com>
2022-06-28 23:32:55 -0300

    Populate e Exec

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,26 +3,30 @@ import livros from "../models/Livro.js";
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            if (err) {
-                res.status(400).send({message: `${err.message} - Erro ao buscar os livros.`});
-            }
-            else {
-                res.status(200).json(livros);
-            }
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
+                if (err) {
+                    res.status(400).send({message: `${err.message} - Erro ao buscar os livros.`});
+                }
+                else {
+                    res.status(200).json(livros);
+                }
         });
     };
 
     static listarLivrosPorId = (req, res) => {
         const id = req.params.id;
 
-        livros.findById(id, (err, livros) => {
-            if (err) {
-                res.status(400).send({message: `${err.message} - Falha ao buscar o livro.`})
-            }
-            else {
-                res.status(200).send(livros);
-            }
+        livros.findById(id) 
+            .populate('autor', 'nome')
+            .exec((err, livros) => {
+                if (err) {
+                    res.status(400).send({message: `${err.message} - Falha ao buscar o livro.`})
+                }
+                else {
+                    res.status(200).send(livros);
+                }
         });
     };
 
